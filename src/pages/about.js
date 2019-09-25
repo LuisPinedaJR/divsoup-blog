219fe03f2a8b6a51b434fe7a19f2a272fa8051c0
Luis Pineda <lspnd00@gmail.com>
2019-09-24 22:51:01 -0400

    divsoup blog first coommit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+const about = () => (
+    <>
+        <h1>About Me</h1>
+        <p>This is my about page</p>
+        <Link to="/">&larr; Home</Link>
+    </>
+)
+
+
+
+export default about

ae7aa558568de41e023b48607408bb5e003a17e7
Luis Pineda <lspnd00@gmail.com>
2019-09-26 21:56:01 -0400

    added new hook to show mdx blog post to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import usePosts from "../hooks/use.posts"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="DivSoup" />
-    <h1>DivSoup Blog</h1>
-    <p>THis is the home page of the blog</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/about">About</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const posts = usePosts()
+  return (
+    <Layout>
+      <SEO title="DivSoup" />
+      <h1>DivSoup Blog</h1>
+      <p>THis is the home page of the blog</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/about">About</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(posts, null, 2)}</pre>
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage

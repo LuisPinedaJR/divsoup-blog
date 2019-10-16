c1b68acb1ade3dcd140452acea55dc4402404da2
Luis Pineda <lspnd00@gmail.com>
2019-10-15 23:49:20 -0400

    heor comp

---
+++ src/components/hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {Link, graphql, useStaticQuery} from 'gatsby'
+
+const Hero = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}

8723a1d70b75fc153b842f9d19a3feff9f4c3db9
Luis Pineda <lspnd00@gmail.com>
2019-10-18 23:48:00 -0400

    test

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import {Link, graphql, useStaticQuery} from 'gatsby'
+import React from "react"
+import styled from "@emotion/styled"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 const Hero = () => {
-    return(
-        <div>
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hero Component</h1>
+    </div>
+  )
 }

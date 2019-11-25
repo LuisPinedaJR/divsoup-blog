d73675d8e68ccc034bbf2f110cb628f9d4c91d0f
Luis Pineda <lspnd00@gmail.com>
2019-11-24 23:40:30 -0500

    added hero component to be cont..

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Hero from "../components/hero"
 import SEO from "../components/seo"
 import usePosts from "../hooks/use-posts"
 import PostPreview from "../components/post-preview"
@@ -8,18 +9,15 @@ import PostPreview from "../components/post-preview"
 const IndexPage = () => {
   const posts = usePosts()
   return (
-    <Layout>
-      <SEO title="DivSoup" />
-      <h1>DivSoup Blog</h1>
-      <p>This is the home page of the blog</p>
-      <Link to="/page-2/">Go to page 2</Link>
-      <Link to="/about">About</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   )
 }
 
